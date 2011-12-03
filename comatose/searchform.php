bba113a150976aed7d692166440be6f651a23ae6
Paul Rohde <paul@paulrohde.com>
2011-12-02 16:15:55 -0800

    Webkit speech enabling for the search box.

--- comatose/searchform.php
+++ comatose/searchform.php
@@ -1,7 +1,7 @@
 <div class="searchform">
 	<form method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
 		<label for="s" class="assistive-text">Search</label>
-		<input type="text" class="field" name="s" class="s" value="<?php the_search_query(); ?>" placeholder="c#, linq, html, code, photography etc.." />
+		<input type="text" class="field" name="s" class="s" value="<?php the_search_query(); ?>" placeholder="c#, linq, html, code, photography etc.." x-webkit-speech />
 		<input type="submit" class="submit add-on" name="submit" class="searchsubmit" value="Search" />
 	</form>
 </div>
